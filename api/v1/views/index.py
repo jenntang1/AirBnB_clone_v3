d803a66b65f129ea967108597afb9861135b9afe
jenntang1 <1039@holbertonschool.com>
2020-04-28 00:41:05 +0000

    [UPDATE] add route /api/v1/stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,11 +1,30 @@
 #!/usr/bin/python3
-""" Creates a index for blueprint """
+""" Creates an url route for blueprint """
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route("/status")
 def status():
     """ Route returns "status": "OK" """
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/api/v1/stats")
+def stats():
+    """ Route returns count of each obj """
+    classes = [Amenity, City, Place, Review, State, User]
+    total = []
+    for class in classes:
+        total.append(storage.count(class))
+    return jsonify(amenities=total[0], cities=total[1],
+                   places=total[2], reviews=total[3],
+                   states=total[4], users=total[5])
