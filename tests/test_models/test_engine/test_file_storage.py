e4e17bf6b55a7bcc9b0987b09347f9719c4b5bcf
jenntang1 <1039@holbertonschool.com>
2020-04-29 19:23:30 +0000

    [Update] Fix test cases

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -68,13 +68,13 @@ test_file_storage.py'])
             self.assertTrue(len(func[1].__doc__) >= 1,
                             "{:s} method needs a docstring".format(func[0]))
 
-    def test_db_get(self):
+    def test_fs_get(self):
         """Test that retrieves one object in a session"""
         first_state_id = list(storage.all(State).values())[0].id
         one_state = storage.get(State, first_state_id)
         self.assertNotEqual(one_state, 0, "Created one object.")
 
-    def test_db_count(self):
+    def test_fs_count(self):
         """Test that counts the number of objects in a class"""
         self.assertIsInstance(storage.count(State), int, "Is a number!")
 
@@ -125,14 +125,14 @@ class TestFileStorage(unittest.TestCase):
             js = f.read()
         self.assertEqual(json.loads(string), json.loads(js))
 
-    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    @unittest.skipIf(models.storage_t == 'db', "not testing db storage")
     def test_get(self):
         """Test that retrieves one object in a session"""
         first_state_id = list(storage.all(State).values())[0].id
         one_state = storage.get(State, first_state_id)
         self.assertNotEqual(one_state, 0, "Created one object.")
 
-    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    @unittest.skipIf(models.storage_t == 'db', "not testing db storage")
     def test_count(self):
         """Test that counts the number of objects in a class"""
         self.assertIsInstance(storage.count(State), int, "Is a number!")
