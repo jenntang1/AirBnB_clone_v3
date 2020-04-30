b334db859887aac5be8fcf81fc4af23d134a45bd
jenntang1 <1039@holbertonschool.com>
2020-04-30 23:16:53 +0000

    [Update] Fix test cases

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -62,6 +62,7 @@ class TestUser(unittest.TestCase):
     def test_is_subclass(self):
         """Test that User is a subclass of BaseModel"""
         user = User()
+        setattr(user, "password", "Michelle")
         self.assertIsInstance(user, BaseModel)
         self.assertTrue(hasattr(user, "id"))
         self.assertTrue(hasattr(user, "created_at"))
@@ -70,6 +71,7 @@ class TestUser(unittest.TestCase):
     def test_email_attr(self):
         """Test that User has attr email, and it's an empty string"""
         user = User()
+        setattr(user, "password", "Michelle")
         self.assertTrue(hasattr(user, "email"))
         if models.storage_t == 'db':
             self.assertEqual(user.email, None)
@@ -79,6 +81,7 @@ class TestUser(unittest.TestCase):
     def test_password_attr(self):
         """Test that User has attr password, and it's an empty string"""
         user = User()
+        setattr(user, "password", "Michelle")
         self.assertTrue(hasattr(user, "password"))
         if models.storage_t == 'db':
             self.assertEqual(user.password, None)
@@ -88,6 +91,7 @@ class TestUser(unittest.TestCase):
     def test_first_name_attr(self):
         """Test that User has attr first_name, and it's an empty string"""
         user = User()
+        setattr(user, "password", "Michelle")
         self.assertTrue(hasattr(user, "first_name"))
         if models.storage_t == 'db':
             self.assertEqual(user.first_name, None)
@@ -97,6 +101,7 @@ class TestUser(unittest.TestCase):
     def test_last_name_attr(self):
         """Test that User has attr last_name, and it's an empty string"""
         user = User()
+        setattr(user, "password", "Michelle")
         self.assertTrue(hasattr(user, "last_name"))
         if models.storage_t == 'db':
             self.assertEqual(user.last_name, None)
@@ -106,6 +111,7 @@ class TestUser(unittest.TestCase):
     def test_to_dict_creates_dict(self):
         """test to_dict method creates a dictionary with proper attrs"""
         u = User()
+        setattr(u, "password", "Michelle")
         new_d = u.to_dict()
         self.assertEqual(type(new_d), dict)
         self.assertFalse("_sa_instance_state" in new_d)
@@ -118,6 +124,7 @@ class TestUser(unittest.TestCase):
         """test that values in dict returned from to_dict are correct"""
         t_format = "%Y-%m-%dT%H:%M:%S.%f"
         u = User()
+        setattr(u, "password", "Michelle")
         new_d = u.to_dict()
         self.assertEqual(new_d["__class__"], "User")
         self.assertEqual(type(new_d["created_at"]), str)
@@ -128,5 +135,6 @@ class TestUser(unittest.TestCase):
     def test_str(self):
         """test that the str method has the correct output"""
         user = User()
+        setattr(user, "password", "Michelle")
         string = "[User] ({}) {}".format(user.id, user.__dict__)
         self.assertEqual(string, str(user))
